31466c12304acb7cb2e53293572f907fddd748cb
RachelyPerez <68482998+rachelyperez31@users.noreply.github.com>
2024-12-05 01:27:00 -0400

    m

---
+++ lib/features/home/presentation/widgets/home_drawer.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+// Sidemenu
+class HomeDrawer extends StatelessWidget {
+  HomeDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer();
+  }
+}
